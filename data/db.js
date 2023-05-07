2e9860806f8b43fdfeec726d606f388647a46ea6
Alper <alper.demir.fener@gmail.com>
2023-05-07 19:33:16 +0300

    Update db.js

--- data/db.js
+++ data/db.js
@@ -3,7 +3,8 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
     host: process.env.HOST,
     dialect: "mysql",
-    storage : "./session.mysql"
+    storage : "./session.mysql",
+    port: process.env.DB_PORT
 })
 
 async function connect() {
