6fc5b6611f41a0e6c2f119002ca3998936e0d394
Alper <alper.demir.fener@gmail.com>
2023-05-07 15:41:40 +0300

    all pages are responsive now

--- public/js/getMessages.js
+++ public/js/getMessages.js
@@ -7,7 +7,7 @@ window.addEventListener("load", async () => {
         if (message.receiverId == document.getElementById("userId").value) {
             content += `
             <div class="flex px-4">
-            <img src="../../public/images/${message.friend.image}" class="w-9 h-9 rounded-full object-cover mr-1 ring-1 ring-orange-500">
+            <img src="../../public/images/${message.friend.image}" class="w-8 h-8 rounded-full object-cover mr-1 ring-1 ring-orange-500">
             <p class="bg-green-100 dark:bg-[#444654] dark:text-white py-1 px-3 inline-block rounded-lg mb-4 max-w-[20rem]">${message.message}</p>
             </div>
             `
